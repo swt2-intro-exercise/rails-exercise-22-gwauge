0c357a7e525d7990146ccd4aeecbc2c54af20125
Georg Auge <georg.auge01@gmail.com>
2022-11-09 18:19:38 +0100

    Fix: filter papers by year
    Resolves #15

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -3,4 +3,6 @@ class Paper < ApplicationRecord
   validates :year, presence: true, numericality: { only_integer: true }
 
   has_and_belongs_to_many :authors
+
+  scope :year, -> (year) { where(year: year) }
 end
