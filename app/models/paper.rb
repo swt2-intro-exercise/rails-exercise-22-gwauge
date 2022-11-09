d15bd87651b0cb2c0a16be98e414aee3cc8cfff6
Georg Auge <georg.auge01@gmail.com>
2022-11-09 10:50:29 +0100

    Fix: relationship error

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -2,5 +2,5 @@ class Paper < ApplicationRecord
   validates :title, :venue, presence: true
   validates :year, presence: true, numericality: { only_integer: true }
 
-  has_and_belongs_to_many :papers
+  has_and_belongs_to_many :authors
 end
